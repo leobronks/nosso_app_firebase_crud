e48e907df5e96c4cabec62da4c6cc894a2cf7c5f
leonardo gabriel de moraes carvalho <leogabriel2012@hotmail.com>
2026-06-19 20:55:32 -0300

    Export App component for Expo Snack compatibility
    Added export default for App to fix rendering in Expo Snack.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { registerRootComponent } from 'expo';
-
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+// 🚀 Adicionado para corrigir a renderização do ecossistema Expo Snack
+export default App;
